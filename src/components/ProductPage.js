bc1c3bb566d199563d1672950e229010ee8bbeb2
dosang12 <knight2671@gmail.com>
2023-03-21 15:15:56 +0900

    결제페이지 추가

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { API_URL } from "../config/constants.js";
+import { Button, message } from "antd";
 
 import axios from "axios";
 import "./ProductPage.css";
@@ -9,47 +10,67 @@ import relativeTime from "dayjs/plugin/relativeTime";
 dayjs.extend(relativeTime);
 
 const ProductPage = () => {
-	const navigate = useNavigate();
-	const { id } = useParams();
-	const [product, setProduct] = useState(null);
-	useEffect(() => {
-		let url = `${API_URL}/products/${id}`;
-		axios
-			.get(url)
-			.then((result) => {
-				console.log(result);
-				setProduct(result.data.product);
-			})
-			.catch((error) => {
-				console.log(error);
-			});
-	}, []);
-	if (product == null) {
-		return <h1>상품정보를 받고 있습니다...</h1>;
-	}
-	return (
-		<div>
-			<button
-				onClick={() => {
-					navigate(-1);
-				}}
-				id="back-btn">
-				뒤로
-			</button>
-			<div id="image-box">
-				<img src={`${API_URL}/${product.imageUrl}`} alt={product.name} />
-			</div>
-			<div id="profile-box">
-				<img src="/images/icons/avatar.png" alt={product.seller} />
-				<span className="product-seller">{product.seller}</span>
-			</div>
-			<div className="content-box">
-				<div id="name">{product.name}</div>
-				<div id="price">{product.price}</div>
-				<div className="product-date">상품등록일: {dayjs(product.createdAt).format("YY년MM월DD일-hh시MM분ss초")}</div>
-				<div id="description">{product.description}</div>
-			</div>
-		</div>
-	);
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+  const getProduct = () => {
+    axios
+      .get(`${API_URL}/products/${id}`)
+      .then((result) => {
+        console.log(result);
+        setProduct(result.data.product);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getProduct();
+  }, []);
+  if (product == null) {
+    return <h1>상품정보를 받고 있습니다...</h1>;
+  }
+
+  const onClickPurchase = () => {
+    axios
+      .post(`${API_URL}/purchase/${id}`)
+      .then((result) => {
+        message.info("결제가 완료 되었습니다");
+        getProduct();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+        id="back-btn"
+      >
+        뒤로
+      </button>
+      <div id="image-box">
+        <img src={`${API_URL}/${product.imageUrl}`} alt={product.name} />
+      </div>
+      <div id="profile-box">
+        <img src="/images/icons/avatar.png" alt={product.seller} />
+        <span className="product-seller">{product.seller}</span>
+      </div>
+      <div className="content-box">
+        <div id="name">{product.name}</div>
+        <div id="price">{product.price}</div>
+        <div className="product-date">상품등록일: {dayjs(product.createdAt).format("YY년MM월DD일-hh시MM분ss초")}</div>
+        <Button size="large" type="primary" danger={true} className="payment" onClick={onClickPurchase}>
+          즉시결제하기
+        </Button>
+        <pre id="description">{product.description}</pre>
+      </div>
+    </div>
+  );
 };
 export default ProductPage;
