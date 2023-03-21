01d70570deea7bdf1ac2abd42d1f01900df5aea2
dosang12 <knight2671@gmail.com>
2023-03-21 15:33:54 +0900

    서버 경로 수정

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NODE_ENV === "production" ? "https://port-0-react-db-3xcah2glbi42tno.gksl2.cloudtype.app" : "http://localhost:8080";
+export const API_URL = process.env.NODE_ENV === "production" ? "https://port-0-mango-shop-server2-p8xrq2mlfg43w7v.sel3.cloudtype.app/" : "http://localhost:8080";
